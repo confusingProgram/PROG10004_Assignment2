dc1b774d6f1901adf6eb3678410d8e2e71e972e2
Andy Do <doandy@sheridancollege.ca>
2023-10-21 14:04:54 -0400

    Created pizza size loop in App

--- App.py
+++ App.py
@@ -35,3 +35,30 @@ while True: # User information loop
     if confirm == "Yes": # User info loop will end if confirmation is "Yes"
         print("Welcome " + username + "!")
         break
+    
+while True: # Pizza size selection
+    size = input("What size pizza would you like? Small, Medium, Large, or X-Large: ")
+    if size == "Small":
+        print("Price per Small pizza: $10.")
+    elif size == "Medium":
+        print("Price per Medium pizza: $12")
+    elif size == "Large":
+        print("Price per Large pizza: $15")
+    elif size == "X-Large":
+        print("Price per X-Large pizza: $18")
+    else:
+        print("Please enter a proper size.")
+        continue
+    
+    while True:
+        confirm = input("Are you sure you want " + size + " size? Yes or No: ")
+        if confirm == "Yes" or confirm == "No":
+            break
+        else:
+            print("Please select Yes or No.")
+    
+    print()
+    
+    if confirm == "Yes":
+        print(size + " size selected.")
+        break
