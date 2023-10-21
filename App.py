0792ab7ccc5cf5a04c5dc10177a954826af14064
Andy Do <doandy@sheridancollege.ca>
2023-10-21 14:00:42 -0400

    Initialized some variables in App and added intro

---
+++ App.py
@@ -0,0 +1,16 @@
+"""This module contains the Application, where the program will interact with the user"""
+import User
+import Pizza
+
+# Placeholder values for information to get from user.
+username = ""
+email = ""
+address = ""
+size = ""
+number_of_pizzas = 0
+confirm = ""
+
+# Welcome text
+print("Stackables: The Best Pizza Around".center(100, " "))
+print()
+print("Thank you for using our service!")
