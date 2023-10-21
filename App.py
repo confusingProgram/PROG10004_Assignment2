da5541e1375fc8fe324d5300a50f4e0dc5866145
Andy Do <doandy@sheridancollege.ca>
2023-10-21 14:04:33 -0400

    Created user info loop in App

--- App.py
+++ App.py
@@ -14,3 +14,24 @@ confirm = ""
 print("Stackables: The Best Pizza Around".center(100, " "))
 print()
 print("Thank you for using our service!")
+
+while True: # User information loop
+    username = input("Enter your name: ")
+    email = input("Enter your email address: ")
+    address = input("Enter the delivery address: ")
+
+    while True: # Confirmation loop
+        print("Your name is: " + username)
+        print("Your email address is: " + email)
+        print("The delivery address is: " + address)
+        confirm = input("Is this correct? Yes or No: ")
+
+        if confirm == "Yes" or confirm == "No": # Confirmation loop will break if "Yes" or "No"
+            break
+        else:
+            print("Please select Yes or No.")
+    print()
+        
+    if confirm == "Yes": # User info loop will end if confirmation is "Yes"
+        print("Welcome " + username + "!")
+        break
